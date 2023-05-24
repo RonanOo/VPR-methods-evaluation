7bdd0709969a0448186f7a6543b16755d3a3fcc1
ronani <ronani@ronanoostveen.nl>
2023-05-24 14:05:39 +0200

    Add transform if doing validation on msls folder

--- test_dataset.py
+++ test_dataset.py
@@ -1,4 +1,3 @@
-
 import os
 import numpy as np
 from glob import glob
@@ -13,19 +12,19 @@ def read_images_paths(dataset_folder):
     'dataset_folder'_images_paths.txt exists, read paths from such file.
     Otherwise, use glob(). Keeping the paths in the file speeds up computation,
     because using glob over large folders might be slow.
-    
+
     Parameters
     ----------
     dataset_folder : str, folder containing JPEG images
-    
+
     Returns
     -------
     images_paths : list[str], paths of JPEG images within dataset_folder
     """
-    
+
     if not os.path.exists(dataset_folder):
         raise FileNotFoundError(f"Folder {dataset_folder} does not exist")
-    
+
     file_with_paths = dataset_folder + "_images_paths.txt"
     if os.path.exists(file_with_paths):
         print(f"Reading paths of images within {dataset_folder} from {file_with_paths}")
@@ -34,14 +33,18 @@ def read_images_paths(dataset_folder):
         images_paths = [dataset_folder + "/" + path for path in images_paths]
         # Sanity check that paths within the file exist
         if not os.path.exists(images_paths[0]):
-            raise FileNotFoundError(f"Image with path {images_paths[0]} "
-                                    f"does not exist within {dataset_folder}. It is likely "
-                                    f"that the content of {file_with_paths} is wrong.")
+            raise FileNotFoundError(
+                f"Image with path {images_paths[0]} "
+                f"does not exist within {dataset_folder}. It is likely "
+                f"that the content of {file_with_paths} is wrong."
+            )
     else:
         print(f"Searching test images in {dataset_folder} with glob()")
         images_paths = sorted(glob(f"{dataset_folder}/**/*.jpg", recursive=True))
         if len(images_paths) == 0:
-            raise FileNotFoundError(f"Directory {dataset_folder} does not contain any JPEG images")
+            raise FileNotFoundError(
+                f"Directory {dataset_folder} does not contain any JPEG images"
+            )
     return images_paths
 
 
@@ -58,10 +61,10 @@ class TestDataset(data.Dataset):
             be considered a positive.
         """
         super().__init__()
-        
+
         self.database_paths = read_images_paths(database_folder)
         self.queries_paths = read_images_paths(queries_folder)
-        
+
         # Read UTM coordinates, which must be contained within the paths
         # The format must be path/to/file/@utm_easting@utm_northing@...@.jpg
         try:
@@ -70,42 +73,62 @@ class TestDataset(data.Dataset):
             utm_east = float(image_path.split("@")[1])
             utm_north = float(image_path.split("@")[2])
         except:
-            raise ValueError("The path of images should be path/to/file/@utm_east@utm_north@...@.jpg "
-                             f"but it is {image_path}, which does not contain the UTM coordinates.")
-        
-        self.database_utms = np.array([(path.split("@")[1], path.split("@")[2]) for path in self.database_paths]).astype(float)
-        self.queries_utms = np.array([(path.split("@")[1], path.split("@")[2]) for path in self.queries_paths]).astype(float)
-        
+            raise ValueError(
+                "The path of images should be path/to/file/@utm_east@utm_north@...@.jpg "
+                f"but it is {image_path}, which does not contain the UTM coordinates."
+            )
+
+        self.database_utms = np.array(
+            [(path.split("@")[1], path.split("@")[2]) for path in self.database_paths]
+        ).astype(float)
+        self.queries_utms = np.array(
+            [(path.split("@")[1], path.split("@")[2]) for path in self.queries_paths]
+        ).astype(float)
+
         # Find positives_per_query, which are within positive_dist_threshold (default 25 meters)
         knn = NearestNeighbors(n_jobs=-1)
         knn.fit(self.database_utms)
-        self.positives_per_query = knn.radius_neighbors(self.queries_utms,
-                                                        radius=positive_dist_threshold,
-                                                        return_distance=False)
-        
+        self.positives_per_query = knn.radius_neighbors(
+            self.queries_utms, radius=positive_dist_threshold, return_distance=False
+        )
+
         self.images_paths = [p for p in self.database_paths]
         self.images_paths += [p for p in self.queries_paths]
-        
+
         self.database_num = len(self.database_paths)
         self.queries_num = len(self.queries_paths)
-        
-        self.base_transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
-    
+
+        self.base_transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
+
+        if "msls" in database_folder:
+            self.base_transform = transforms.Compose(
+                [
+                    transforms.Resize(size=(480, 640)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                    ),
+                ]
+            )
+
     def __getitem__(self, index):
         image_path = self.images_paths[index]
         pil_img = Image.open(image_path).convert("RGB")
         normalized_img = self.base_transform(pil_img)
         return normalized_img, index
-    
+
     def __len__(self):
         return len(self.images_paths)
-    
+
     def __repr__(self):
         return f"< #queries: {self.queries_num}; #database: {self.database_num} >"
-    
+
     def get_positives(self):
         return self.positives_per_query
-
